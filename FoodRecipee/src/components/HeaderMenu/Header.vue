9219e339c4636a1adf6261e4f2d64dae048d4838
Mr.Nilsson <hampus10699@gmail.com>
2025-10-03 15:20:59 +0200

    styled and added links to navbar

--- FoodRecipee/src/components/HeaderMenu/Header.vue
+++ FoodRecipee/src/components/HeaderMenu/Header.vue
@@ -8,11 +8,12 @@ const { styles } = useFixedHeader(headerRef)
 </script>
 
 <template>
-    <header class="Header" ref="headerRef" :style="styles">
-    <RouterLink to="/">Hem</RouterLink>
-    <RouterLink to="/recipes">Recept</RouterLink>
-    <RouterLink to="/loginPage">Logga in</RouterLink>
-    <RouterLink to="/about">Om</RouterLink>
+    <header class="Header" ref="headerRef" :style="styles" >
+    <RouterLink to="/" class="hover-underline">Hem</RouterLink>
+    <RouterLink to="/recipes" class="hover-underline">Recept</RouterLink>
+    <RouterLink to="/myPage" class="hover-underline">Min sida</RouterLink>
+    <RouterLink to="/loginPage" class="hover-underline">Logga in</RouterLink>
+    <RouterLink to="/about" class="hover-underline">Om</RouterLink>
   </header>
 </template>
 
@@ -26,7 +27,6 @@ const { styles } = useFixedHeader(headerRef)
   width: 50%;
   padding: 1rem;
   display: flex;
-  align-items: center;
   gap: 1rem;
   background-color: #52c864;
   z-index: 1000; /* Ensure the header is above other content */
@@ -38,5 +38,35 @@ const { styles } = useFixedHeader(headerRef)
   margin-top: 120px; /* To prevent content from being hidden under the fixed header */
   padding: 1rem;
   height: 2000px; /* Or any other large value to create a scrollable area */
+}
+.hover-underline {
+  font-size: 1.3rem;
+  color: #ffffff;
+  position: relative;
+  display: inline-block;
+}
+
+.hover-underline::after,
+.hover-underline::before {
+  content: '';
+  position: absolute;
+  width: 100%;
+  height: 2px;
+  background: linear-gradient(to right, #408080, #00ffff);
+  bottom: -5px;
+  left: 0;
+  transform: scaleX(0);
+  transform-origin: right;
+  transition: transform 0.4s ease-out;
+}
+
+.hover-underline::before {
+  top: -5px;
+  transform-origin: left;
+}
+
+.hover-underline:hover::after,
+.hover-underline:hover::before {
+  transform: scaleX(1);
 }
 </style>
--- FoodRecipee/src/router/index.js
+++ FoodRecipee/src/router/index.js
@@ -2,6 +2,7 @@ import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
 import RecipeView from '@/views/RecipeView.vue'
 import LogInView from '@/views/LogInView.vue'
+import MyPageView from '@/views/MyPageView.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -28,6 +29,11 @@ const router = createRouter({
       path: '/loginPage',
       name: 'login',
       component: LogInView,
+    },
+    {
+      path: '/myPage',
+      name: 'mypage',
+      component: MyPageView,
     },
   ],
 })
